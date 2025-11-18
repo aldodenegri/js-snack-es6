28dc5a27db638b00a0fb2678b0c4fd727f8d8779
aldodenegri <aldodenegri00@gmail.com>
2025-11-18 17:52:52 +0100

    inserimento di oggetto bici e di elementi nuovi

--- snack1.js
+++ snack1.js
@@ -1,3 +1,17 @@
 export function scriviQualcosa1(){
+    let biciObj=
+        [
+            {nome:"Mountain Bike",peso:"20"},
+            {nome:"Bici Flash",peso:"15"}
+        ];
+        const nomeNuovo=prompt("Inserisci nome bici");
+        const pesoNuovo=parseInt(prompt("Inserisci peso bici (in Kg)"));
+        biciObj.push(
+            {
+                nome:nomeNuovo,
+                peso:pesoNuovo
+            }
+        );
+        console.log(biciObj);
     return "Ciao sono snack1";
 }
