fc83fa479ddd3db9eb9c63ff375e37551d927ca9
aldodenegri <aldodenegri00@gmail.com>
2025-11-18 17:59:39 +0100

    calcolo bici leggera

--- snack1.js
+++ snack1.js
@@ -12,6 +12,17 @@ export function scriviQualcosa1(){
                 peso:pesoNuovo
             }
         );
-        console.log(biciObj);
-    return "Ciao sono snack1";
+        let biciLeggera=biciObj[0];
+        for(let i=1; i<biciObj.length;i++)//salto il primo che è già verificato dalla riga scritta sopra
+        {
+            if(biciObj[i].peso<biciLeggera.peso)
+            {
+                biciLeggera=biciObj[i];
+            }
+        }
+        console.log(biciLeggera);
+    return `
+        <h2>Snack 1 - Bici più leggera</h2>
+        <p>La bici più leggera è: <strong>${biciLeggera.nome}</strong> con ${biciLeggera.peso} kg</p>
+    `;
 }
