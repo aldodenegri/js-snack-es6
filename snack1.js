de89c18fa28ceec495a0509b18c0039781c27cda
aldodenegri <aldodenegri00@gmail.com>
2025-11-18 18:54:23 +0100

    correzione di denominazione del peso di biciObj

--- snack1.js
+++ snack1.js
@@ -1,8 +1,8 @@
 export function scriviQualcosa1() {
     let biciObj =
         [
-            { nome: "Mountain Bike", peso: "20" },
-            { nome: "Bici Flash", peso: "15" }
+            { nome: "Mountain Bike", peso: 20 },
+            { nome: "Bici Flash", peso: 15 }
         ];
     const nomeNuovo = prompt("Inserisci nome bici");
     const pesoNuovo = parseInt(prompt("Inserisci peso bici (in Kg)"));
